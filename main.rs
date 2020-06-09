4e007546d82b4d6d97fd2264a97e815650f017bb
Alexandre Piel <alexandre.piel@gmail.com>
2020-06-09 22:14:53 +0200

    up

--- main.rs
+++ main.rs
@@ -77,6 +77,12 @@ fn compile_specifier_as_module_resolve_callback<'a>(
   let mut cbs = v8::CallbackScope::new_escapable(context);
   let mut hs = v8::EscapableHandleScope::new(cbs.enter());
   let scope = hs.enter();
+
+
+  let specifier_str = specifier.to_rust_string_lossy(scope);
+  println!("specifier_str {:?}", specifier_str);
+
+
   let origin = mock_script_origin(scope, "module.js");
   let source = v8::script_compiler::Source::new(specifier, &origin);
   let module = v8::script_compiler::compile_module(scope, source).unwrap();
