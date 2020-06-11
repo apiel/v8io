df789b7d1bc00c9834f18f963359bb8915e52004
Alexandre Piel <alexandre.piel@gmail.com>
2020-06-11 15:23:55 +0200

    try try try

--- modules/module_map.rs
+++ modules/module_map.rs
@@ -15,3 +15,31 @@ pub fn insert(identity_hash: i32, module_item: ModuleItem) {
         .unwrap()
         .insert(identity_hash, module_item);
 }
+
+pub fn get(identity_hash: i32) -> Option<&'static ModuleItem> {
+    MODULE_MAP.lock().unwrap().get(&identity_hash).clone() //.cloned() //.clone() //.unwrap()
+}
+
+// pub fn get<'a>(identity_hash: &'a i32) -> Option<&'a ModuleItem> {
+// pub fn get(identity_hash: i32) -> Option<&'static ModuleItem> {
+// pub fn get(identity_hash: &'static i32) -> Option<&'static ModuleItem> {
+//     // pub fn get(identity_hash: i32) -> Option<ModuleItem> {
+//     MODULE_MAP.lock().unwrap().get(identity_hash).clone() //.cloned() //.clone() //.unwrap()
+// }
+
+// // pub fn get_absolute_path(identity_hash: i32) -> std::string::String {
+// pub fn get_absolute_path(identity_hash: i32) -> &'static str {
+//     let module_item = MODULE_MAP.lock().unwrap().get(&identity_hash);
+//     match module_item {
+//         Some(item) => return item.absolute_path.to_string().as_ref(),
+//         None => return "",
+//     };
+// }
+
+// pub fn get_absolute_path(identity_hash: i32) -> std::string::String {
+//     let module_item = MODULE_MAP.lock().unwrap().get(&identity_hash);
+//     match module_item {
+//         Some(item) => return item.absolute_path.to_string(),
+//         None => return "".to_string(),
+//     };
+// }
